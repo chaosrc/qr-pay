182aa7921f7cc8714123f014cd1436814be2311b
chao <971064906@qq.com>
2019-05-14 01:07:21 +0800

    Add build

---
+++ build/precache-manifest.d9f3394614bd12e85eed9883b1cbb0fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037f08c5e6bfe7a93b2be013d96ca13c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4fce706f74683855e3d",
+    "url": "./static/css/main.ae73db54.chunk.css"
+  },
+  {
+    "revision": "46cbd9aa874bea722358",
+    "url": "./static/js/2.a88bc227.chunk.js"
+  },
+  {
+    "revision": "a4fce706f74683855e3d",
+    "url": "./static/js/main.2b43bba6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
